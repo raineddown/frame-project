f2b85e6af1ca80ad20828746e237cb5b16679d34
gzx <2113280896@qq.com>
2020-12-03 23:34:58 +0800

    new

---
+++ src/main/java/com/example/demo/service/DeptService.java
@@ -0,0 +1,28 @@
+package com.example.demo.service;
+
+
+import com.example.demo.entity.SysDept;
+import com.example.demo.vo.req.DeptAddReqVO;
+import com.example.demo.vo.req.DeptUpdateReqVO;
+import com.example.demo.vo.resp.DeptRespNodeVO;
+
+import java.util.List;
+
+/**
+ * @ClassName: DeptService
+ * TODO:类文件简单描述
+ * @Version: 0.0.1
+ */
+public interface DeptService {
+
+    List<SysDept> selectAll();
+
+    List<DeptRespNodeVO> deptTreeList(String deptId);
+
+    SysDept addDept(DeptAddReqVO vo);
+
+    void updateDept(DeptUpdateReqVO vo);
+
+    void deletedDept(String id);
+
+}
